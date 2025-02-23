fae544a184918d9807eec347d6432372ae85ac43
kathipallimadhukiran <22a91a04l9@aec.edu.in>
2025-02-24 01:31:07 +0530

    update pages.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
-"use client";
+"use client";  // Ensure the component runs only on the client-side
 
-import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
 import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
+const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
 
 export default function Home() {
   const [articles, setArticles] = useState([]);
@@ -16,25 +17,21 @@ export default function Home() {
   useEffect(() => {
     async function getData() {
       try {
-        const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
-
+        const res = await fetch(`https://dev.to/api/articles?username=yourDevUsername`);
         if (!res.ok) {
           throw new Error("Failed to fetch data");
         }
-
         const data = await res.json();
-        const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-        setArticles(filtered);
+        setArticles(data);
       } catch (error) {
         console.error("Error fetching articles:", error);
       }
     }
-
     getData();
   }, []);
 
   return (
-    <div suppressHydrationWarning>
+    <div>
       <HeroSection />
       <AboutSection />
       <Experience />
