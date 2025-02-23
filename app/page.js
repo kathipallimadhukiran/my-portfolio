ae8031954eaaf0832495a183638a54ec7b8682d7
kathipallimadhukiran <22a91a04l9@aec.edu.in>
2025-02-24 01:27:11 +0530

    update pages.j

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
@@ -6,25 +8,33 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import { useEffect, useState } from "react";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+export default function Home() {
+  const [articles, setArticles] = useState([]);
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
 
-  const data = await res.json();
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
 
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+        const data = await res.json();
+        const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+        setArticles(filtered);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      }
+    }
 
-  return filtered;
-};
-
-export default async function Home() {
+    getData();
+  }, []);
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -33,5 +43,5 @@ export default async function Home() {
       <Education />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
