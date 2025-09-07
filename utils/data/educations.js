907523f837ee620dcf4347ff728f97b4fdb4a8b9
KATHIPALLI MADHU KIRAN <119720465+kathipallimadhukiran@users.noreply.github.com>
2025-09-07 23:54:18 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -9,13 +9,14 @@ export const educations = [
     id: 2,
     title: "Higher Secondary Certificate",
     duration: "2020 - 2022",
-    institution: "Sri Chaitanya Junior College  Rajahmundry",
+    institution: "Sri Chaitanya Junior College , Rajahmundry",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
     duration: "2010 - 2020",
-    institution: "Saraswathi Vidya Nikethan School Rajahmundry",
+    institution: "Saraswathi Vidya Nikethan School , Rajahmundry",
   }
 
 ]
+
