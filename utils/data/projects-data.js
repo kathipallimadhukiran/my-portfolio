796889166e9d59314953a9d6e23ab3e2b4e06f6a
KATHIPALLI MADHU KIRAN <119720465+kathipallimadhukiran@users.noreply.github.com>
2025-09-07 23:50:25 +0530

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -16,19 +16,23 @@ export const projectsData = [
         role: 'Front-End Developer',
         code: '',
         demo: '',
+    },
+    {
+        id: 3,
+        name: 'Smart Faculty Availability Tracker',
+        description: "Led a 4-member team in building a mobile app to track real-time faculty availability at Aditya University. Developed RESTful APIs for schedule management, substitutions, and live updates. Integrated push notifications for instant alerts, optimized database queries for performance, and published a research paper based on the project.",
+        tools: ['React Native', 'Node.js', 'Express.js', 'MongoDB', 'Bootstrap', 'Firebase'],
+        role: 'Team Leader / Full Stack Developer',
+        code: '',
+        demo: '',
+    },
+    {
+        id: 4,
+        name: 'Service Kart',
+        description: "Developed and deployed RESTful APIs for user authentication, service listings, and booking management. Integrated Razorpay API for secure in-app payments and automated Gmail API notifications to enhance user engagement. Designed efficient MongoDB schemas and aggregation pipelines while collaborating with front-end developers for smooth integration.",
+        tools: ['React Native', 'Node.js', 'Express.js', 'MongoDB', 'Razorpay API', 'Gmail API'],
+        role: 'Back-End Developer',
+        code: '',
+        demo: '',
     }
 ];
-
-
-// Do not remove any property.
-// Leave it blank instead as shown below
-
-// {
-//     id: 1,
-//     name: '',
-//     description: "",
-//     tools: [],
-//     role: '',
-//     code: '',
-//     demo: '',
-// },
